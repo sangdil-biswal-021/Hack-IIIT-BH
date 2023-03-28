650976afe504b2276a1516e67222fe109a645dc6
Roshan Dash <roshan1337d@gmail.com>
2023-03-28 18:14:23 +0530

    header anim

--- src/routes/homePage/index.js
+++ src/routes/homePage/index.js
@@ -2,6 +2,7 @@ import classes from "./index.module.css";
 import { useLottie } from "lottie-react";
 import logoAnim from "../../assets/logoAnimated.json";
 import { HashLink } from "react-router-hash-link";
+import { InView } from 'react-intersection-observer';
 
 export default function HomePage() {
     const options = {
@@ -13,15 +14,23 @@ export default function HomePage() {
     return (
         <>
             <section className={classes.headerSection}>
-                <div className={classes.lottieAnim}>{View}</div>
-                <div className={classes.date}>XX - YY JULY 2023</div>
-                <div className={classes.headerDesc}>Unleash your creativity and problem-solving skills in a weekend of innovation, collaboration, and fun. Showcase your talent, win prizes, and make a difference with cutting-edge technology!</div>
-                <HashLink className={classes.btnWrap} to="#">
-                    <div className={classes.svgicon} id="svg-6" data-svg-icon="assets/img/icons/shape.svg"><svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 188 61">
-                        <path className={classes.btnPath} d="M874,554.154L893.08,524h146.67L1060,552.843,1039.75,583H893.08Z" transform="translate(-873 -523)" ></path>
-                    </svg></div>
-                    <span>REGISTER</span>
-                </HashLink>
+                <InView triggerOnce>{({ inView, ref }) => (
+                    <div className={classes.lottieAnim} ref={ref} style={{ animation: inView ? `${classes.goUp} 1s forwards .3s ease` : "none" }}>{View}</div>
+                )}</InView>
+                <InView triggerOnce>{({ inView, ref }) => (
+                    <div className={classes.date} ref={ref} style={{ animation: inView ? `${classes.goUp} 1s forwards .6s ease` : "none" }}>XX - YY JULY 2023</div>
+                )}</InView>
+                <InView triggerOnce>{({ inView, ref }) => (
+                    <div className={classes.headerDesc} ref={ref} style={{ animation: inView ? `${classes.goUp} 1s forwards .9s ease` : "none" }}>Unleash your creativity and problem-solving skills in a weekend of innovation, collaboration, and fun. Showcase your talent, win prizes, and make a difference with cutting-edge technology!</div>
+                )}</InView>
+                <InView triggerOnce>{({ inView, ref }) => (
+                    <HashLink className={classes.btnWrap} to="#" ref={ref} style={{ animation: inView ? `${classes.scaleToFull} 1s forwards 1.2s` : "none" }}>
+                        <div className={classes.svgicon} id="svg-6" data-svg-icon="assets/img/icons/shape.svg"><svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 188 61">
+                            <path className={classes.btnPath} d="M874,554.154L893.08,524h146.67L1060,552.843,1039.75,583H893.08Z" transform="translate(-873 -523)" ></path>
+                        </svg></div>
+                        <span>REGISTER</span>
+                    </HashLink>
+                )}</InView>
             </section>
 
             <section className={classes.sponsorsScrollWrap}>
